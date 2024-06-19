1bc956f02770711756f323d56bf2b9d906e20ad3
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-06-19 11:09:27 +0500

    again

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="w-full py-[2%] flex justify-between px-[3%] text-black items-center overflow-hidden ">
-      <div className="h-14 w-40 ">
+      <div className="h-14 w-40 hidden lg:block">
         <img
           src="/logo.png"
           alt="logo"
@@ -16,7 +16,7 @@ const Header = () => {
         <li className="cursor-pointer  hover:font-[500]">Preise</li>
         <li className="cursor-pointer  hover:font-[500]">Häufige Fragen</li>
       </ul>
-      <div>
+      <div className="hidden lg:bloc">
         <button className="font-[500]">Login</button>
         <button className="bg-[#F0CF90] hover:px-8 duration-500 py-3 px-6 ml-9 rounded-full font-[500]">
           Registrieren
