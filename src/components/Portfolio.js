6f8bab29c2b0d0ee592b7dc288094dd55db7cc72
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-06-19 11:15:46 +0500

    profile fixed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,25 +14,21 @@ const Portfolio = () => {
         />
         <div className="flex-1  space-y-4">
           <h3 className="text-2xl md:text-3xl font-[700]">Moses, 22 Jahre</h3>
-          <div className="px-[10%] lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
+          <div className="px-[10%] relative lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
             <p className="border-b-2 border-red-50">Follower </p>
-            <div className="relative">
-              <span className="bg-red-500 absolute bottom-0 left-0 h-[6px] w-full rounded-full "></span>
-            </div>
+            <span className="bg-[#AD4545]  absolute top-7 left-0 h-[6px] ml-[10%] w-[30%] rounded-full "></span>
+
             <p>674</p>
           </div>
-          <div className="px-[10%] lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
+          <div className="px-[10%] relative lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
             <p className="border-b-2 border-red-50">⌀ Likes </p>
-            <div className="relative">
-              <span className="bg-red-500 absolute bottom-0 left-0 h-[6px] w-full rounded-full "></span>
-            </div>
+            <span className="bg-[#4F5665]  absolute top-7 left-0 h-[6px] ml-[10%] w-[20%] rounded-full "></span>
             <p>213</p>
           </div>
-          <div className="px-[10%] lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
+          <div className="px-[10%] relative lg:pr-[50%] text-lg md:text-xl font-[700] flex justify-between">
             <p className="border-b-2 border-red-50"> ⌀ Kommentare</p>
-            <div className="relative">
-              <span className="bg-red-500 absolute bottom-0 left-0 h-[6px] w-full rounded-full "></span>
-            </div>
+            <span className="bg-purple-600  absolute top-7 left-0 h-[6px] ml-[10%] w-[10%] rounded-full "></span>
+
             <p>11</p>
           </div>
           <h3 className="text-3xl md:text-5xl font-[700] pt-14">
